1313b365c3d5418ca1aebf9a0f869a74f973327c
Bennett <bennybenester@gmail.com>
2021-04-23 15:20:28 +0300

    fix naming in imported modules

--- src/modules/review/controllers/review.controller.ts
+++ src/modules/review/controllers/review.controller.ts
@@ -1,7 +1,7 @@
 import { Controller } from '@nestjs/common';
 import { BaseController } from '../../../core/controllers/base.controller';
 import { Review } from '../entities/review.entity';
-import { ReviewService } from '../services/Review.service';
+import { ReviewService } from '../services/review.service';
 
 @Controller('api/' + Review.plural)
 export class ReviewController extends BaseController<Review> {
