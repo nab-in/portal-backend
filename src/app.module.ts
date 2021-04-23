cf06a41a7035dac90082109ccad58f14f909f4f2
Bennett <bennybenester@gmail.com>
2021-04-23 22:36:36 +0300

    add heroku configs

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,7 +6,30 @@ import { modules } from './modules/modules.export';
 
 @Module({
   // imported modules definition
-  imports: [...modules, TypeOrmModule.forRoot()],
+  imports: [
+    ...modules,
+    TypeOrmModule.forRoot({
+      name: 'default',
+      type: 'postgres',
+      host: 'ec2-18-215-111-67.compute-1.amazonaws.com',
+      port: 5432,
+      username: 'dfckkdjtmnzblu',
+      password:
+        'e0917d22f2db1c884f949f328f5d6404ddfba8f4f7323fde6878b06066aa106e',
+      database: 'd5nek5j478bf4',
+      synchronize: true,
+      logging: false,
+      dropSchema: true,
+      entities: ['dist/modules/**/entities/*.entity.js'],
+      migrations: ['dist/database/migrations/**/*.js'],
+      subscribers: ['src/subscriber/**/*.ts'],
+      cli: {
+        entitiesDir: 'src/modules/entities',
+        migrationsDir: 'src/database/migrations',
+        subscribersDir: 'src/subscriber',
+      },
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
