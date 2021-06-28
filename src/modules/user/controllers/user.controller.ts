71cd1543eeac0032718b4a398e27a9c481082fb2
Bennett <bennybenester@gmail.com>
2021-06-28 19:42:52 +0300

    add guards to user routes
    Signed-off-by: Bennett <bennybenester@gmail.com>

--- src/modules/user/controllers/user.controller.ts
+++ src/modules/user/controllers/user.controller.ts
@@ -3,6 +3,7 @@ import {
   Controller,
   Get,
   HttpStatus,
+  Param,
   Post,
   Query,
   Req,
@@ -18,6 +19,7 @@ import { BaseController } from '../../../core/controllers/base.controller';
 import { resolveResponse } from '../../../core/resolvers/response.sanitizer';
 import {
   genericFailureResponse,
+  getSuccessResponse,
   postSuccessResponse,
 } from '../../../core/utilities/response.helper';
 import { User } from '../entities/user.entity';
@@ -122,5 +124,58 @@ export class UserController extends BaseController<User> {
       jobs: jobs.map((job) => resolveResponse(job)),
     };
     return res.status(HttpStatus.OK).send(response);
+  }
+  @Get()
+  @UseFilters(new HttpErrorFilter())
+  @UseGuards(AuthGuard('jwt'))
+  async findAll(@Query() query): Promise<any> {
+    if (query.paging === 'false') {
+      const allContents: User[] = await this.service.findAll();
+      return {
+        [User.plural]: (allContents || []).map((contents) =>
+          resolveResponse(contents),
+        ),
+      };
+    }
+
+    const pagerDetails: any = getPagerDetails(query);
+
+    const [entityRes, totalCount]: [User[], number] =
+      await this.service.findAndCount(
+        query.fields,
+        query.filter,
+        pagerDetails.pageSize,
+        pagerDetails.page - 1,
+      );
+
+    return {
+      pager: {
+        ...pagerDetails,
+        pageCount: entityRes.length,
+        total: totalCount,
+        nextPage: `/api/${User.plural}?page=${+pagerDetails.page + +'1'}`,
+      },
+      [User.plural]: (entityRes || []).map((contents) =>
+        resolveResponse(contents),
+      ),
+    };
+  }
+
+  @Get(':id')
+  @UseGuards(AuthGuard('jwt'))
+  @UseFilters(new HttpErrorFilter())
+  async findOneUser(
+    @Res() res: any,
+    @Param() params,
+    @Query() query,
+  ): Promise<any> {
+    const results = await this.service.findOneByUid(params.id, query.fields);
+    if (results) {
+      return getSuccessResponse(res, resolveResponse(results));
+    } else {
+      return res.status(HttpStatus.NOT_FOUND).send({
+        message: `Item with identifier ${params.id} could not be found`,
+      });
+    }
   }
 }
