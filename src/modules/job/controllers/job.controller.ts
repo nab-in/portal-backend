93d0cb47d9415248d2ed145c60f1356977077d46
Bennett <bennybenester@gmail.com>
2021-07-04 22:34:29 +0300

    update jobs to add attachments see #71

--- src/modules/job/controllers/job.controller.ts
+++ src/modules/job/controllers/job.controller.ts
@@ -1,13 +1,19 @@
 import {
+  Body,
   Controller,
   Delete,
+  Get,
   HttpStatus,
+  NotFoundException,
   Param,
   Post,
   Req,
   Res,
+  UploadedFile,
+  UploadedFiles,
   UseFilters,
   UseGuards,
+  UseInterceptors,
 } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 import { HttpErrorFilter } from '../../../core/interceptors/error.filter';
@@ -16,6 +22,12 @@ import { genericFailureResponse } from '../../../core/utilities/response.helper'
 import { User } from '../../user/entities/user.entity';
 import { Job } from '../entities/job.entity';
 import { JobService } from '../services/job.service';
+import { FileInterceptor, FilesInterceptor } from '@nestjs/platform-express';
+import { diskStorage } from 'multer';
+import {
+  editFileName,
+  imageFileFilter,
+} from '../../../core/helpers/sanitize-image';
 
 @Controller('api/' + Job.plural)
 export class JobController extends BaseController<Job> {
@@ -30,14 +42,14 @@ export class JobController extends BaseController<Job> {
     @Res() res: any,
     @Param() param: any,
   ): Promise<any> {
-      const user: User = await this.service.findUser(req.user.id);
-      const job = await this.service.findOneByUid(param.job);
-      const createdEntity = await this.service.apply({ job, user });
-      if (createdEntity !== undefined) {
-        return res.status(HttpStatus.OK).send(createdEntity);
-      } else {
-        return genericFailureResponse(res);
-      }
+    const user: User = await this.service.findUser(req.user.id);
+    const job = await this.service.findOneByUid(param.job);
+    const createdEntity = await this.service.apply({ job, user });
+    if (createdEntity !== undefined) {
+      return res.status(HttpStatus.OK).send(createdEntity);
+    } else {
+      return genericFailureResponse(res);
+    }
   }
   @Delete(':job/revoke')
   @UseGuards(AuthGuard('jwt'))
@@ -47,13 +59,71 @@ export class JobController extends BaseController<Job> {
     @Res() res: any,
     @Param() param: any,
   ): Promise<any> {
-      const user: User = await this.service.findUser(req.user.id);
-      const job = await this.service.findOneByUid(param.job);
-      const createdEntity = await this.service.revoke({ job, user });
-      if (createdEntity !== undefined) {
-        return res.status(HttpStatus.OK).send(createdEntity);
-      } else {
-        return genericFailureResponse(res);
-      }
+    const user: User = await this.service.findUser(req.user.id);
+    const job = await this.service.findOneByUid(param.job);
+    const createdEntity = await this.service.revoke({ job, user });
+    if (createdEntity !== undefined) {
+      return res.status(HttpStatus.OK).send(createdEntity);
+    } else {
+      return genericFailureResponse(res);
+    }
+  }
+
+  @Post('upload')
+  @UseGuards(AuthGuard('jwt'))
+  @UseInterceptors(
+    FileInterceptor('image', {
+      storage: diskStorage({
+        destination: 'src/files',
+        filename: editFileName,
+      }),
+      fileFilter: imageFileFilter,
+    }),
+  )
+  async uploadedFile(@UploadedFile() file, @Req() req, @Body() body: any) {
+    const job = await this.service.findOneByUid(body.company);
+
+    const response = {
+      originalname: file.originalname,
+      filename: file.filename,
+    };
+    if (job) {
+      job['attachment'] = file.filename;
+      await this.service.update(job);
+      return response;
+    } else {
+      throw new NotFoundException(
+        `Job with ID ${body.company} could not be found`,
+      );
+    }
+  }
+
+  @Post('multiple')
+  @UseGuards(AuthGuard('jwt'))
+  @UseInterceptors(
+    FilesInterceptor('image', 20, {
+      storage: diskStorage({
+        destination: 'src/files',
+        filename: editFileName,
+      }),
+      fileFilter: imageFileFilter,
+    }),
+  )
+  async uploadMultipleFiles(@UploadedFiles() files) {
+    const response = [];
+    files.forEach((file) => {
+      const fileReponse = {
+        originalname: file.originalname,
+        filename: file.filename,
+      };
+      response.push(fileReponse);
+    });
+    return response;
+  }
+  @Get(':imgpath/attachment')
+  @UseGuards(AuthGuard('jwt'))
+  seeUploadedFile(@Param('imgpath') image, @Res() res) {
+    console.log('imgpath');
+    return res.sendFile(image, { root: 'src/files' });
   }
 }
--- src/modules/job/job.module.ts
+++ src/modules/job/job.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { MulterModule } from '@nestjs/platform-express/multer';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from '../user/entities/user.entity';
 import { JobCategoryController } from './controllers/job-category.controller';
@@ -8,7 +9,12 @@ import { Job } from './entities/job.entity';
 import { JobCategoryService } from './services/job-category.service';
 import { JobService } from './services/job.service';
 @Module({
-  imports: [TypeOrmModule.forFeature([Job, JobCategory, User])],
+  imports: [
+    TypeOrmModule.forFeature([Job, JobCategory, User]),
+    MulterModule.register({
+      dest: 'src/files',
+    }),
+  ],
   controllers: [JobController, JobCategoryController],
   providers: [JobService, JobCategoryService],
 })
