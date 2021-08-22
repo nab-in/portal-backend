7bebef9a4c58c9bb4c28611d391149af262c05dc
Bennett <bennybenester@gmail.com>
2021-08-22 11:23:49 +0300

    select user roles in login

--- src/modules/user/services/jwt.strategy.service.ts
+++ src/modules/user/services/jwt.strategy.service.ts
@@ -22,11 +22,13 @@ export class JwtPassportStrategy extends PassportStrategy(Strategy) {
       if (payload.email) {
         user = await this.userRepository.findOne({
           where: { email: payload.email },
+          relations: ['userRoles'],
         });
       }
       if (payload.username) {
         user = await this.userRepository.findOne({
           where: { username: payload.username },
+          relations: ['userRoles'],
         });
       }
       if (!user) {
