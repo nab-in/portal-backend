cad3876bba55943d89662e4d42a0fac6d43032e4
Bennett <bennybenester@gmail.com>
2021-08-22 12:39:57 +0300

    update endpoints for posting and updating attachments

--- src/modules/company/controllers/company.controller.ts
+++ src/modules/company/controllers/company.controller.ts
@@ -1,5 +1,20 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  NotFoundException,
+  Param,
+  Post,
+  Req,
+  UploadedFile,
+  UseGuards,
+  UseInterceptors,
+} from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import { FileInterceptor } from '@nestjs/platform-express';
+import * as fs from 'fs';
+import { diskStorage } from 'multer';
 import { BaseController } from 'src/core/controllers/base.controller';
+import { editFileName, imageFileFilter } from 'src/core/helpers/sanitize-image';
+import { getConfiguration } from 'src/core/utilities/systemConfigs';
 import { Company } from '../entities/company.entity';
 import { CompanyService } from '../services/company.service';
 
@@ -7,5 +22,45 @@ import { CompanyService } from '../services/company.service';
 export class CompanyController extends BaseController<Company> {
   constructor(private service: CompanyService) {
     super(service, Company);
+  }
+  @Post(':id/logo')
+  @UseGuards(AuthGuard('jwt'))
+  @UseInterceptors(
+    FileInterceptor('', {
+      storage: diskStorage({
+        destination: getConfiguration().company,
+        filename: editFileName,
+      }),
+      fileFilter: imageFileFilter,
+    }),
+  )
+  async uploadedCv(
+    @UploadedFile() file: any,
+    @Req() req: any,
+    @Param() params: { id: string },
+  ) {
+    try {
+      const path: string = '/api/Companies/' + file.filename + '/cv';
+      const response = {
+        path,
+        message: 'Company Logo saved successfully',
+      };
+      const company: Company = await this.service.findOneByUid(params.id);
+      const oldLogo = company?.logo;
+      if (company.id) {
+        company.logo = file.filename;
+        await this.service.update(company);
+        if (oldLogo && oldLogo !== 'logo.png') {
+          fs.unlinkSync(getConfiguration().company + '/' + oldLogo);
+        }
+        return response;
+      } else {
+        throw new NotFoundException(
+          `Company with ID ${company.id} could not be found`,
+        );
+      }
+    } catch (e) {
+      throw new Error(e.message);
+    }
   }
 }
